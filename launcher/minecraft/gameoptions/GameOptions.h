55d0e4c2c3b8e8b57a088882ca72f82cec2129bf
Tayou <tayou@gmx.net>
2023-02-27 17:30:30 +0100

    cleaned up and improved GameOptions Model & Page
    - added array support
    - cleaned up logic
    - ran clang-format
    - added description & default value columns
    - added basic editing support (bools only) - no saving
    
    Co-authored-by: TheLastRar <TheLastRar@users.noreply.github.com>
    Signed-off-by: Tayou <tayou@gmx.net>

--- launcher/minecraft/gameoptions/GameOptions.h
+++ launcher/minecraft/gameoptions/GameOptions.h
@@ -2,6 +2,7 @@
 /*
  *  Prism Launcher - Minecraft Launcher
  *  Copyright (C) 2023 Tayou <tayou@gmx.net>
+ *  Copyright (C) 2023 TheLastRar <TheLastRar@users.noreply.github.com>
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
